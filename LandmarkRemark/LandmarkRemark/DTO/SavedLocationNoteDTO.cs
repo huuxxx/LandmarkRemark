b5873da9f71b9eb91a7daddb165cf4d80bb872c0
Huxley Dowling <hdowling87@hotmail.com>
2020-11-29 19:14:36 +1000

    Added base controller. Added Entity ORM package. Added base DTO. Remove project sample.

---
+++ LandmarkRemark/LandmarkRemark/DTO/SavedLocationNoteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandmarkRemark.DTO
+{
+    public class SavedLocationNoteDTO
+    {
+        public string UserID { get; set; }
+        public string LocationCoordinates { get; set; }
+
+        public string MessageNote { get; set; }
+    }
+}
